521b31fdc9d7c6d3cc159b399011291e73087985
Clarissa Flores <clarissaflores@Clarissas-MacBook-Pro.local>
2025-12-02 22:27:44 -0600

    push new workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://clarissa-flores.github.io/portfolio98',
+  base: '/portfolio98/'
+});
